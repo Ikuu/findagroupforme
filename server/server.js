49fdd0dc034d2494a12995430281990ffef7fbfb
Alexander Andrew Anderson <revolution6000@gmail.com>
2014-11-10 15:58:02 +0000

    Moved server.js to server folder

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // Routes
-var group = require('./server/routes/group');
-var user = require('./server/routes/user');
+var group = require('./routes/group');
+var user = require('./routes/user');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
