fe7978e7b7c965b809f31cebad0725db77ca017d
Alexander Andrew Anderson <revolution6000@gmail.com>
2014-11-10 21:15:31 +0000

    Passport Tutorial
    Completed passport tutorial to see working implementation. Cleaned up
    Front

--- client/angularApp/controllers/main.js
+++
@@ -1,4 +0,0 @@
-angular.module('MyApp')
-  .controller('MainCtrl', ['$scope', 'User', function($scope, User) {
-    $scope.users = User.query();
-  }]);
--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 var mongoose = require('mongoose');
+var session = require('express-session')
 
 // Routes
 var group = require('./routes/group');
@@ -14,6 +16,8 @@ var app = express();
 app.use('/api/groups/', group);
 app.use('/api/users/', user);
 
+require('./config/passport')(passport); // pass passport for configuration
+
 // move this to another file
 mongoose.connect('mongodb://192.168.1.64/honoursTest');
 
@@ -23,6 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
+
+// Passport Tutorial stuff, change.
+app.use(session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+require('./routes/auth.js')(app, passport);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
