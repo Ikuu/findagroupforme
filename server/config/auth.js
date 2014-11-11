7be7d75530c751e5b3a34c82a0356dd1cbd4d259
Alexander Andrew Anderson <revolution6000@gmail.com>
2014-11-11 18:42:58 +0000

    Empty Auth added.

---
+++ server/config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+	'twitterAuth': {
+		'consumerKey'       : '',
+		'consumerSecret'    : '',
+		'callbackURL'       : 'http://127.0.0.1:3000/auth/twitter/callback'
+	},
+	'facebookAuth': {
+		'clientID'			: '',
+		'clientSecret'		: '',
+		'callbackURL'		: "http://localhost:3000/auth/facebook/callback"
+	},
+	'googleAuth': {
+		'clientID'			: '',
+		'clientSecret'		: '',
+		'callbackURL'		: "http://localhost:3000/auth/google/callback"
+	}
+}
