0ceea9c2da36ebbdf3620f1855e8dd3152d35a59
Alexander Andrew Anderson <revolution6000@gmail.com>
2014-11-24 20:46:54 +0000

    minor change
    Fixed error, data was added to DB as string causing search to break.

--- server/controllers/auth/auth.group.js
+++ server/controllers/auth/auth.group.js
@@ -5,7 +5,7 @@ exports.checkIfLoggedIn = function(req, res, next){
 }
 
 exports.hasGroupPermission = function(req, res, next){
-	Group.find({_id: req.body._id}, {members: req.session.passport.user}).exec(function(err, group){
+	Group.find({_id: req.body._id, members: req.session.passport.user}).exec(function(err, group){
 		group.length == 0 ? res.status(403).end() : next();
 	});
 }
