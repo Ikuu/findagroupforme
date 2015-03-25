81b30dcd5a21002086e780d151283809c0b1de8a
Alexander Anderson <revolution6000@gmail.com>
2015-03-25 19:23:53 +0000

    updated gitignore

--- client/app/modules/core/controllers/site.controller.js
+++ client/app/modules/core/controllers/site.controller.js
@@ -9,7 +9,6 @@ angular.module('app.core')
 	setInterval(loadUserDetails, 20000);
 
 	function loadUserDetails() {
-		console.log(1);
 		UserFactory.getUser().then(function success(response) {
 			$scope.loggedInUser = response.data;
 			if (response.data.username) {
